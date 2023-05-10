c345e607d48f264101c3b61546c5e504e209056b
c97Ernesto <ernesto-cafre@outlook.com>
2023-05-10 12:47:58 -0300

    firebase

--- src/main/java/com/backend/ernesto/WebConfig.java
+++ src/main/java/com/backend/ernesto/WebConfig.java
@@ -11,6 +11,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**").allowedOrigins("http://localhost:4200/");
+		registry.addMapping("/**").allowedOrigins("https://portfoliobackend-wc8a.onrender.com");
 	}
 }
